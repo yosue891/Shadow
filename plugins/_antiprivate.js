06652bd88c047c5a3984c830bc20067e4948a9e6
Ell Contrerax <sarmientodieg@gmail.com>
2025-09-21 17:09:35 -0500

    Actualizar _antiprivate.js

--- plugins/_antiprivate.js
+++ plugins/_antiprivate.js
@@ -1,4 +1,4 @@
-export async function before(m, { conn, isAdmin, isBotAdmin, isMods, isROwner }) {
+export async function before(m, { conn, isAdmin, isBotAdmin, isROwner }) {
 if (m.isBaileys && m.fromMe) return !0
 if (m.isGroup) return !1
 if (!m.message) return !0
@@ -7,7 +7,7 @@ if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TI
 const chat = global.db.data.chats[m.chat]
 const bot = global.db.data.settings[conn.user.jid] || {}
 if (m.chat === '120363401404146384@newsletter') return !0
-if (bot.antiPrivate && !isMods) {
+if (bot.antiPrivate) {
 await m.reply(`ꕥ Hola @${m.sender.split`@`[0]}, mi dueño a desactivado los comandos en los chats privados el cual serás bloqueado, si quieres usar los comandos del bot te invito a que te unas a nuestra comunidad.\n\n${community}`, false, {mentions: [m.sender]})
 await this.updateBlockStatus(m.chat, 'block')
 }
