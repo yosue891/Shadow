7f5627d19623db22016fe4cc6fa3fc4b04d9dd74
Ell Contrerax <sarmientodieg@gmail.com>
2025-09-21 17:07:55 -0500

    Actualizar _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,7 +1,7 @@
 const linkRegex = /(chat\.whatsapp\.com\/[0-9A-Za-z]{20,24})|(z?https:\/\/whatsapp\.com\/channel\/[0-9A-Za-z]{20,24})/i
 const allowedLinks = ['https://whatsapp.com/channel/0029Vb64nWqLo4hb8cuxe23n']
 
-export async function before(m, { conn, isAdmin, isBotAdmin, isMods, isROwner, participants }) {
+export async function before(m, { conn, isAdmin, isBotAdmin, isROwner, participants }) {
 if (!m.isGroup) return
 if (!m || !m.text) return
 const chat = global?.db?.data?.chats[m.chat]
@@ -14,7 +14,7 @@ if (isBotAdmin) {
 const linkThisGroup = `https://chat.whatsapp.com/${await conn.groupInviteCode(m.chat)}`
 if (isGroupLink && m.text.includes(linkThisGroup)) return !0
 }
-if (chat.antilink && isGroupLink && !isAdmin && isBotAdmin && !isMods && m.key.participant !== conn.user.jid) {
+if (chat.antilink && isGroupLink && !isAdmin && isBotAdmin && m.key.participant !== conn.user.jid) {
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: m.key.id, participant: m.key.participant }})
 await conn.reply(m.chat, `> ꕥ Se ha eliminado a *${global.db.data.users[m.key.participant].name || 'Usuario'}* del grupo por \`Anti-Link\`, no permitimos enlaces de *${isChannelLink ? 'canales' : 'otros grupos'}*.`, null)
 await conn.groupParticipantsUpdate(m.chat, [m.key.participant], 'remove')
